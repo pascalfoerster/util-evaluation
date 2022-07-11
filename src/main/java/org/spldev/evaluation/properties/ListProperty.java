19e44ae36a85c68d14f0322040c944e8527a5b2d
Elias Kuiter <info@elias-kuiter.de>
2022-07-11 13:39:01 +0200

    Updated license headers

--- src/main/java/org/spldev/evaluation/properties/ListProperty.java
+++ src/main/java/org/spldev/evaluation/properties/ListProperty.java
@@ -1,23 +1,23 @@
 /* -----------------------------------------------------------------------------
- * Evaluation Lib - Miscellaneous functions for performing an evaluation.
- * Copyright (C) 2021  Sebastian Krieter
+ * evaluation - Utilities for reproducible evaluations
+ * Copyright (C) 2021 Sebastian Krieter
  * 
- * This file is part of Evaluation Lib.
+ * This file is part of evaluation.
  * 
- * Evaluation Lib is free software: you can redistribute it and/or modify it
+ * evaluation is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License,
+ * the Free Software Foundation, either version 3.0 of the License,
  * or (at your option) any later version.
  * 
- * Evaluation Lib is distributed in the hope that it will be useful,
+ * evaluation is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  * 
  * You should have received a copy of the GNU Lesser General Public License
- * along with Evaluation Lib.  If not, see <https://www.gnu.org/licenses/>.
+ * along with evaluation. If not, see <https://www.gnu.org/licenses/>.
  * 
- * See <https://github.com/skrieter/evaluation> for further information.
+ * See <https://github.com/FeatJAR/evaluation> for further information.
  * -----------------------------------------------------------------------------
  */
 package org.spldev.evaluation.properties;
