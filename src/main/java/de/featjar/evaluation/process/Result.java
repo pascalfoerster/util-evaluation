c82ddb738e30e4e596f055a50757d777f787bb3d
Sebastian Krieter <sebastian.krieter@uni-ulm.de>
2023-09-29 15:19:47 +0200

    Update licence headers

--- src/main/java/de/featjar/evaluation/process/Result.java
+++ src/main/java/de/featjar/evaluation/process/Result.java
@@ -1,7 +1,7 @@
 /*
- * Copyright (C) 2023 Sebastian Krieter
+ * Copyright (C) 2023 FeatJAR-Development-Team
  *
- * This file is part of evaluation.
+ * This file is part of FeatJAR-evaluation.
  *
  * evaluation is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with evaluation. If not, see <https://www.gnu.org/licenses/>.
  *
- * See <https://github.com/FeatJAR/evaluation> for further information.
+ * See <https://github.com/FeatJAR> for further information.
  */
 package de.featjar.evaluation.process;
 
--- src/main/java/de/featjar/evaluation/util/ModelReader.java
+++ src/main/java/de/featjar/evaluation/util/ModelReader.java
@@ -1,22 +1,22 @@
 /*
- * Copyright (C) 2023 Sebastian Krieter
+ * Copyright (C) 2023 FeatJAR-Development-Team
  *
- * This file is part of FeatJAR-util-evaluation.
+ * This file is part of FeatJAR-evaluation.
  *
- * util-evaluation is free software: you can redistribute it and/or modify it
+ * evaluation is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3.0 of the License,
  * or (at your option) any later version.
  *
- * util-evaluation is distributed in the hope that it will be useful,
+ * evaluation is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with util-evaluation. If not, see <https://www.gnu.org/licenses/>.
+ * along with evaluation. If not, see <https://www.gnu.org/licenses/>.
  *
- * See <https://github.com/FeatureIDE/FeatJAR-evaluation> for further information.
+ * See <https://github.com/FeatJAR> for further information.
  */
 package de.featjar.evaluation.util;
 
